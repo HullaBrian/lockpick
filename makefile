8e74be7bf5b51c0cc17df0d38120dce5cc2f169a
HullaBrian <beierlejc@gmail.com>
2023-10-27 12:48:48 -0500

    Basic help page and usage verification

--- makefile
+++ makefile
@@ -1,7 +1,8 @@
 pick : main.o
-	gcc out/main.o -o out/pick
+	gcc out/main.o -o pick
 
 main.o : src/main.c
+	mkdir out -p
 	gcc -Wall -c src/main.c -o out/main.o
 
 clean:
--- src/main.c
+++ src/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <string.h>
 
-int main (int argc, char* argcv[]) {
-    printf("E\n");
+#define INVALID_USAGE "Invalid usage. Use 'pick --help' for a usages options.\n"
+
+int verify_valid_call (int argc, char* argv[]) {
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {  // detect if current argv is a parameter
+            switch(argv[i][1]) {
+                case 'h':
+                case 'H':
+                case 'w':
+                case 'a':
+                case 't':
+                    break;
+                default:
+                    return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int main (int argc, char* argv[]) {
+    if (argc == 1) {
+        printf(INVALID_USAGE);
+        return 2;
+    }
+
+    if (strcmp(argv[0], "--help") == 0) {
+        printf("Usage: pick [options]...\n");
+        printf("\nOptions:\n");
+
+        printf("  -h             pass the literal hash of the password\n");
+        printf("  -H             pass a file containing 1 or more hashed passwords\n");
+        printf("  -w             wordlist to check against the password(s)\n");
+        printf("  -a             supported hashing algorithm to use:\n");
+        printf("                   sha256, md5\n");
+        printf("  -t             number of threads to use\n");
+        return 1;
+    }
+
+    if (verify_valid_call(argc, argv) == 0) {
+        printf(INVALID_USAGE);
+        return 2;
+    }
+    printf("Success!\n");
+
     return 1;
 }
